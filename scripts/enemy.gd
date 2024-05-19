1c92e8cb56bb843b957d0c60812138448dd42791
GeloxGero <gelo12gero@gmail.com>
2024-05-20 02:54:34 +0800

    ai

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -6,7 +6,7 @@ extends CharacterBody2D
 const SPEED = 20.0
 const JUMP_VELOCITY = -400.0
 
-
+var raycast_detection
 
 var player
 var is_player_near = false
@@ -29,14 +29,15 @@ var context_map = [0,0,0,0,0,0,0,0]
 var best_direction_index = 0
 
 
-
 enum States{
 	LURK,
 	CHASE,
 	INACTIVE,
 	HIDE
 }
+
 var state = States.INACTIVE
+var wall_direction_index = 0
 #custom functions
 func update_context_map(player_position, current_position):
 	#function normalizes vector automatically
@@ -47,7 +48,8 @@ func update_context_map(player_position, current_position):
 		interest_array[i] = vector_to_player.dot(arr[i])
 	
 	if state == States.HIDE:
-		process_wall_direction()
+		interest_array[wall_direction_index] += 5
+
 	
 	for i in range(8):
 		context_map[i] = interest_array[i] - danger_array[i]
@@ -55,20 +57,27 @@ func update_context_map(player_position, current_position):
 			best_direction_index = 0
 		elif context_map[i] > context_map[best_direction_index]:
 			#for some reason subtracting one makes it more accurate 
+
 			best_direction_index = i - 1
 			
 
 func process_wall_direction():
-	var distances = $RaycastDetection.distances
+	var distances = raycast_detection.distances
 	var closest_direction_index = 0
 	for i in range(8):
 		if distances[i] == 0:
 			continue
 		if distances[i] < distances[closest_direction_index]:
 			closest_direction_index = i
-			
-	interest_array[closest_direction_index] += 5
-	print(closest_direction_index)
+	wall_direction_index = closest_direction_index
+
+func move_within_wall():
+	if raycast_detection.collision_count > 4 and raycast_detection.collision_count < 8:
+		var distances = raycast_detection.distances
+		var out_index = 0
+		for i in range(8):
+			if distances[i] == 0:
+				interest_array[i] += 5
 
 #godot functions
 func _physics_process(delta):
@@ -77,7 +86,6 @@ func _physics_process(delta):
 		$ScreechAudio.play()
 		$ScreechCD.start(5)
 		state = States.CHASE
-		print(state)
 		
 	
 	if player:
@@ -95,26 +103,24 @@ func _physics_process(delta):
 		
 		#print("Player Spotted")
 		if state == States.CHASE:
-			print("Chase")
 			if abs(global_position.distance_to(player.global_position)) <= 40 and can_jumpscare:
 				$AnimationPlayer.play("bite")
 				await $AnimationPlayer.animation_finished
 				state = States.HIDE
 				velocity = Vector2(0,0)
 				can_jumpscare = false
+				process_wall_direction()
 
 			velocity = velocity + ((steering_force * sharp_turn) * delta)
 			position += velocity
 			if velocity > Vector2.ZERO:
 				$AnimationPlayer.play("swim")
 		if state == States.HIDE:
-			print("Hide")
 			if can_jumpscare:
 				state = States.LURK
 			velocity = velocity + ((steering_force * sharp_turn) * (delta / 3))
 			position += velocity
 		if state == States.LURK:
-			print("Lurk")
 			var distance = abs(global_position.distance_to(player.global_position))
 			if distance > 80:
 				velocity = velocity + ((steering_force * sharp_turn) * delta)
@@ -126,14 +132,17 @@ func _on_player_detection_body_entered(body):
 	is_player_near = true 
 	can_jumpscare = true
 	player = body
-	$RaycastDetection.enable_collisions()
 	state = States.LURK
+	if !raycast_detection:
+		raycast_detection = $RaycastDetection
+		raycast_detection.enabled = true
 
 
 func _on_player_detection_body_exited(body):
 	is_player_near = false
 	player = null
-	$RaycastDetection.enabled = false
+	raycast_detection.enabled = false
+	raycast_detection = null
 
 
 func _on_player_detection_area_entered(area):
@@ -152,12 +161,9 @@ func _on_player_detection_area_exited(area):
 
 
 func _on_screech_cd_timeout():
-	print("TIMEOUT")
 	if player:
-		print("HAS PLAYER")
 		if state == States.CHASE:
 			return
-		print("LURKING")
 		state = States.LURK
 	else:
 		state = States.INACTIVE
